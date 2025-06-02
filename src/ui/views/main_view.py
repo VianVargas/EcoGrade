224fcddb7387dcc361d9e068f13d828cf1533536
SunnySideD0wn <152190326+SunnySideD0wn@users.noreply.github.com>
2025-06-02 20:51:49 +0800

    update

--- src/ui/views/main_view.py
+++ src/ui/views/main_view.py
@@ -19,7 +19,11 @@ class MainView(QWidget):
     def initUI(self):
         layout = QHBoxLayout()
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
         layout.setSpacing(30)  # Make left and right panels closer
+=======
+        layout.setSpacing(10)  # Make left and right panels closer
+>>>>>>> Stashed changes
 =======
         layout.setSpacing(10)  # Make left and right panels closer
 >>>>>>> Stashed changes
@@ -35,8 +39,13 @@ class MainView(QWidget):
         
         # Set fixed size for camera widgets
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
         CAMERA_WIDTH = 300
         CAMERA_HEIGHT = 280
+=======
+        CAMERA_WIDTH = 320
+        CAMERA_HEIGHT = 240
+>>>>>>> Stashed changes
 =======
         CAMERA_WIDTH = 320
         CAMERA_HEIGHT = 240
